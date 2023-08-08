4887c9ce45a9b6f0cdcf66965ae2be2c33fab533
SYNically-ACKward <ryan@thenetworkguy.org>
2023-08-08 14:49:06 -0400

    snake to camel conversion utility

--- zsdk/utilities.py
+++ zsdk/utilities.py
@@ -5,22 +5,20 @@ import urllib3
 from .logger import setup_logger
 
 # Disable the InsecureRequestWarning
-urllib3.disable_warnings(
-    category=urllib3.exceptions.InsecureRequestWarning
-)
+urllib3.disable_warnings(category=urllib3.exceptions.InsecureRequestWarning)
 
 logger = setup_logger(name=__name__)
 
 
 def call(
-        session: requests.Session,
-        method,
-        url,
-        retries: int = 5,
-        wait_time: int = 5,
-        silence_logs: bool = False,
-        verify: bool = False,
-        **kwargs
+    session: requests.Session,
+    method,
+    url,
+    retries: int = 5,
+    wait_time: int = 5,
+    silence_logs: bool = False,
+    verify: bool = False,
+    **kwargs,
 ):
     for attempt in range(retries + 1):
         try:
@@ -31,6 +29,13 @@ def call(
             if not silence_logs:
                 logger.error(f"Encountered Error: {e}")
             if attempt < retries:
-                logger.debug(f"Retrying request in {wait_time}s. Retries remaining: {retries - attempt}")
+                logger.debug(
+                    f"Retrying request in {wait_time}s. Retries remaining: {retries - attempt}"
+                )
                 time.sleep(wait_time)
     raise requests.RequestException("Max Retries Reached.")
+
+
+def snake_to_camel(snake_str):
+    components = snake_str.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
